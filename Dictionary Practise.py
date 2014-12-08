cde996e043a4f310507b11a0ce140c2cc8edbf81
Callum A Horton <hortonc5@uni.coventry.ac.uk>
2014-12-08 08:53:41 +0000

    Dictionary Practise 1

---
+++ Dictionary Practise.py
@@ -0,0 +1,4 @@
+CDOA = {'Callum' : 'Callum Horton, 117 Wichnor Road, West Midlands, England', 'Jake' : 'Jake Burgess, 156 Lemon View, West Midlands, England', 'Marcel' : 'Marcel Loeve, 1 Golden Cove, South Coast, South Africa', 'Mohamed' : 'Mohamed Hasan, 19 Glasgow Street, Scotland', 'Thomas' : 'Thomas Basset, 34 Hood Road, West Midlands, England', 'Dom' : 'Dom Tuck, 4 Terraria Land, The World',}
+
+query1 = raw_input ("Enter who you are searching for. First Name only ")
+print CDOA[query1]
